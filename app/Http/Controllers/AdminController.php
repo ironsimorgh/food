4f2e631621bc25da706f8aff1081fb791993cfef
ali <seyedaliseyfi@gmail.com>
2025-04-18 16:35:08 +0330

    forgetpassword complited

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -69,5 +69,28 @@ class AdminController extends Controller
         \Mail::to($request->email)->send(new Websitemail($subject,$message));
         return redirect()->back()->with('success', 'Reset Password Link Send On Your Email');
     }
+
+    public function AdminResetPassword($token,$email){
+        $admin_data = Admin::where('email',$email)->where('token',$token)->first();
+        if (!$admin_data) {
+            return redirect()->route('admin.login')->with('error','invalid token or email');
+        }
+        return view('admin.reset_password',compact('token','email'));
+    }
+
+    public function AdminResetPasswordSubmit(Request $request){
+        $request->validate([
+            'password' => 'required',
+            'password_confirmation' => 'required|same:password',
+        ]);
+        $admin_data = Admin::where('email',$request->email)->where('token',$request->token)->first();
+        $admin_data->password = Hash::make($request->password);
+        $admin_data->token="";
+        $admin_data->update();
+
+        return redirect()->route('admin.login')->with('success','Password Reset Successfully');
+    }
+
+
 }
 
