5da10c48f600f8740f0f132b05fb7287c82f1c70
unknown <seyedaliseyfi@gmail.com>
2025-12-26 02:08:30 +0330

    comment and review 6

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Models\Client;
 use App\Models\Menu;
 use App\Models\Gllery;
+use App\Models\Review;
 use App\Models\Wishlist;
 use Carbon\Carbon;
 
@@ -19,9 +20,24 @@ class HomeController extends Controller
             return $menu->products->isNotEmpty();
         });
         $gallerys = Gllery::where('client_id',$id)->get();
-        return view('frontend.details_page',compact('client','menus','gallerys'));
+        $reviews = Review::where('client_id',$client->id)->get();
+        $totalReviews = $reviews->count();
+        $ratingSum = $reviews->sum('rating');
+        $averageRating = $totalReviews > 0 ? $ratingSum / $totalReviews : 0;
+        $roundedAverageRating = round($averageRating, 1);
+        $ratingCounts = [
+            '5' => $reviews->where('rating',5)->count(),
+            '4' => $reviews->where('rating',4)->count(),
+            '3' => $reviews->where('rating',3)->count(),
+            '2' => $reviews->where('rating',2)->count(),
+            '1' => $reviews->where('rating',1)->count(),
+        ];
+        $ratingPercentages = array_map(function ($count) use ($totalReviews){
+            return $totalReviews > 0 ? ($count/$totalReviews) * 100 : 0;
+        },$ratingCounts);
+        return view('frontend.details_page',compact('client','menus','gallerys','reviews','roundedAverageRating','totalReviews','ratingCounts','ratingPercentages'));
     }
-    //End Method
+    //End Method 
 
     public function AddWishList(Request $request, $id){
         if(Auth::check()){
